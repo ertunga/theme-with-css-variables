3896515cddcbbaf353cb8f99beb4de0ee149833f
ertungabezirgan <ertungabezirgan@gmail.com>
2022-07-10 17:42:01 +0300

    theme service with local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs';
+import { ThemeService } from './theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [CommonModule]
 })
-export class AppComponent {
-  title = 'theme';
+export class AppComponent implements OnInit {
+
+  constructor(protected themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    const theme = localStorage.getItem('theme');
+    this.themeService.set(theme || 'light');
+
+    this.themeService.activeTheme$.pipe(tap(console.log)).subscribe(themeName => localStorage.setItem('theme', themeName));
+  }
 }
---
+++ src/app/theme.ts
@@ -0,0 +1,5 @@
+export const theme: {[key: string]: string} = {
+    light: 'light-theme',
+    dimmed: 'dimmed-theme',
+    dark: 'dark-theme'
+}
